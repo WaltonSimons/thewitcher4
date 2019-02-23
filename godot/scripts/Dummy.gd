9f232ef0d1e715b630443190be45a061ad17a1ed
WaltonSimons <masteroskar@gmail.com>
2019-02-23 15:33:57 +0100

    Health bars added.

--- godot/scripts/Dummy.gd
+++ godot/scripts/Dummy.gd
@@ -1,8 +1,15 @@
 extends KinematicBody2D
 
-export (float) var hit_points = 100
+signal health_changed(value, max_value)
+
+export (float) var max_hit_points = 100
+var hit_points
+
+func _ready():
+	hit_points = max_hit_points
 
 func take_hit(value):
 	hit_points -= value
+	emit_signal('health_changed', hit_points, max_hit_points)
 	if hit_points <= 0:
 		queue_free()
