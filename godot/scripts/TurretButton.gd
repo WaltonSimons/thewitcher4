8aef6a6d03b742afce14838daddc6c5a4478f576
nukeop <nukeop@gumblert.tech>
2019-02-23 23:03:00 +0100

    get turret icon from prefab

--- godot/scripts/TurretButton.gd
+++ godot/scripts/TurretButton.gd
@@ -3,6 +3,9 @@ extends TextureButton
 signal turret_bought(turret_type, turret_icon)
 export (PackedScene) var turret_prefab
 
+func _ready():
+	get_child(0).texture = turret_prefab.instance().get_child(1).texture
+
 func _gui_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		emit_signal("turret_bought", turret_prefab, $TurretIcon.texture)
--- godot/test2.gd
+++
@@ -1,9 +0,0 @@
-extends Path2D
-onready var follow = get_node("PathFollow2D")
-
-func _ready():
-	set_process(true)
-	pass
-
-func _process(delta):
-	follow.set_offset(follow.get_offset() + 300*delta)
