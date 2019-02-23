fc0c2135840455b2922a0a3225150b79f36933d9
WaltonSimons <masteroskar@gmail.com>
2019-02-23 15:36:49 +0100

    Changed health bar change function name.

--- godot/scripts/HealthBar.gd
+++ godot/scripts/HealthBar.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-func _on_Dummy_health_changed(value, max_value):
+func _on_health_changed(value, max_value):
 	var target_scale = $GreenBar.scale
 	target_scale.x = value/max_value
 	$Tween.interpolate_property($GreenBar, 'scale', $GreenBar.scale, target_scale, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
